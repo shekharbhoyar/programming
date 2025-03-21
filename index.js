19bce0bb999f4afbca7c8afe0e8610d1389df007
chandrashekhar <shekharbhoyar10@gmail.com>
2025-03-21 23:20:58 +0530

    all about functions

---
+++ index.js
@@ -0,0 +1,21 @@
+//1.sum of natural numbers
+//FUNCTION DECLARATION
+// function sumofnaturalnumber() {
+//   return "hiii";
+// }
+// const hello = sumofnaturalnumber();
+// console.log(hello);
+
+//FUNCTION DEFINITION
+// const sumofnaturalnumber = function () {
+//   return "hiii";
+// };
+// const hello = sumofnaturalnumber();
+// console.log(hello);
+
+//
+// const sum = () => {
+//   return "hiiii";
+// };
+// const hello = sum();
+// console.log(hello);
