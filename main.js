4f0d55beca68ed6ba9c6761f59584149a34d470d
chandrashekhar <shekharbhoyar10@gmail.com>
2025-05-06 16:03:49 +0530

    factors

--- main.js
+++ main.js
@@ -1,23 +1,30 @@
 let pr = prompt("give any number");
-      let n = Number(pr);
-      if (pr === null) {
-        console.log("cancelled");
-      } else {
-        if (n > 0) {
-          let primehai = true;
+let n = Number(pr);
+if (pr === null) {
+  console.log("cancelled");
+} else {
+  if (n > 0) {
+    //   let primehai = true;
 
-          for (let i = 2; i <= Math.floor(n / 2); i++) {
-            if (n % i === 0) {
-              primehai = false;
-              break;
-            }
-          }
+    //   for (let i = 2; i <= Math.floor(n / 2); i++) {
+    //     if (n % i === 0) {
+    //       primehai = false;
+    //       break;
+    //     }
+    //   }
 
-          console.log(primehai);
-        } else if (n <= 0) {
-          console.log("number should be positive and above zero");
-        }
-        if (isNaN(n)) {
-          console.log("invalid input");
-        }
+    //   console.log(primehai);
+
+    for (let i = 0; i <= Math.floor(n / 2); i++) {
+      if (n % i === 0) {
+        console.log(i);
       }
+    }
+    console.log(n);
+  } else if (n <= 0) {
+    console.log("number should be positive and above zero");
+  }
+  if (isNaN(n)) {
+    console.log("invalid input");
+  }
+}
