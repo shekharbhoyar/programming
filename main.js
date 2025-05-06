baaf12b26814a4fc3cf9a074972e6118d2b187f6
chandrashekhar <shekharbhoyar10@gmail.com>
2025-05-06 15:55:50 +0530

    html file added

---
+++ main.js
@@ -0,0 +1,23 @@
+let pr = prompt("give any number");
+      let n = Number(pr);
+      if (pr === null) {
+        console.log("cancelled");
+      } else {
+        if (n > 0) {
+          let primehai = true;
+
+          for (let i = 2; i <= Math.floor(n / 2); i++) {
+            if (n % i === 0) {
+              primehai = false;
+              break;
+            }
+          }
+
+          console.log(primehai);
+        } else if (n <= 0) {
+          console.log("number should be positive and above zero");
+        }
+        if (isNaN(n)) {
+          console.log("invalid input");
+        }
+      }
